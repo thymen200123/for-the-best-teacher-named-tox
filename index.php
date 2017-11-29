4c9960ad9d58060aed15d6618031d22afdda09c4
thymen vd bijl <32477932+thymen200123@users.noreply.github.com>
2017-11-29 20:04:00 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+<header>
+	<nav>
+		<div class="main-wrapper">
+			<ul>
+				<li><a href="index.php">home</a></li>
+			</ul>
+			<div class="nav-login">
+				<form>
+					<input type="text" name="uid" placeholder="Username/e-mail">
+					<input type="password" name="pwd" placeholder="password">
+					<button type="submit" name="submit" >login</button>
+				</form>
+				<a href="signup.php">Sign up</a>
+			</div>	
+		</div>
+	</nav>
+</header>
+
+<section>
+	
+</section>
+
+</body>
+</html>
